e0b2c706084ec674bd86260645385691af417ded
Maisa Preis <maisapierinipreis@gmail.com>
2025-09-11 11:32:19 -0300

    deploy: updating build

--- dist/assets/MaiteM-CFky7snt.js
+++
@@ -1,1 +0,0 @@
-import{I as e}from"./MaiteBase-2wwXwHvl.js";import{d as t,k as o,o as r}from"./index-CDmvpbFq.js";import"./gift-2-DPxGAIEM.js";import"./FooterPage-D6NEybIh.js";const _=t({__name:"MaiteM",setup(a){return(i,n)=>(r(),o(e,{giftSize:"M"}))}});export{_ as default};
